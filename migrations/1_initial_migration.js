08a7f90c8e97f3fea0bad8ba7c57314cbbe8ac1c
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-10-13 10:21:07 +0700

    Marketplace.sol

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var Marketplace = artifacts.require("./Marketplace.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Marketplace);
 };
